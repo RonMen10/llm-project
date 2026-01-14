b755a67d54298fb3c0bb3abb8a40a37e624882c8
Ronald Mendez <ronald.mendez@iis.fraunhofer.de>
2026-01-14 17:46:01 +0100

    Directory name formating v2

--- scripts/inference.py
+++ scripts/inference.py
@@ -4,7 +4,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 device = "cuda" if torch.cuda.is_available() else "cpu"
 class Inference():
-    def __init__(self, model_path="./my-finetuned-model"):
+    def __init__(self, model_path="./my_finetuned_model"):
         self.tokenizer = AutoTokenizer.from_pretrained(model_path)
         self.model = AutoModelForCausalLM.from_pretrained(
             model_path,
